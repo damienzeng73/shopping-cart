9cc27735df2c5d241f9c3ad05bae5773fa3b462e
damnee562 <damnee562@gmail.com>
2017-11-22 19:36:37 +0800

    Add quantity if product is already in cart.

--- frontend/src/reducers/Cart.js
+++ frontend/src/reducers/Cart.js
@@ -1,9 +1,21 @@
 import { ADD_TO_CART } from '../constants/ActionTypes'
 import shortid from 'shortid'
+import _ from 'lodash'
 
 const cart = (state=[], action={}) => {
     switch(action.type) {
         case ADD_TO_CART:
+            if (_.filter(state, (obj) => obj.product.name === action.payload.product.name).length > 0) {
+                let newState = state.slice()
+                for (let i=0; i<newState.length; i++) {
+                    if (newState[i].product.name === action.payload.product.name) {
+                        newState[i].quantity += action.payload.quantity
+                    }
+                }
+
+                return newState
+            }
+
             return [
                 ...state,
                 {
