a3b6c5d35448187d0671127075badc18f6babf95
damnee562 <damnee562@gmail.com>
2017-12-07 22:01:37 +0800

    Fix unexpected EOF.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -27,3 +27,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
