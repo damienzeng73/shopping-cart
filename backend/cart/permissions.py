3263eaa76e83ca017d3f8e08b8f54274112714b8
damnee562 <damnee562@gmail.com>
2017-12-11 23:54:53 +0800

    Allow PUT request for decreasing the quantity of products.

--- backend/cart/permissions.py
+++ backend/cart/permissions.py
@@ -7,8 +7,10 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request,
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
+        # so we'll always allow GET, HEAD or OPTIONS requests,
+        # due to we need to decrease the quantity of the product,
+        # we'll always allow PUT requests too.
+        if request.method in permissions.SAFE_METHODS or request.method == 'PUT':
             return True
 
         # Write permissions are only allowed to the owner of the snippet.
